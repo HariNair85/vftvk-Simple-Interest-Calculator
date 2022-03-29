df286b6621da364fb2bd6c14bce27d53d5bae3df
Harikrishnan M <40897366+HariNair85@users.noreply.github.com>
2022-03-29 23:34:48 +0530

    Update

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 function compute()
 {
     let amount = document.getElementById("principal").value;
-    let noofyears = document.getElementById("noofyears").value;
-    let interestrate = document.getElementById("inRange").value;
-    let interest = 0;
-/*     for (let index = 1; index <= noofyears ; index++) {
-        total = total + ( total * ( interestrate / 100 ) );
-    } */
-    interest = ( parseInt(amount) * ( 1 + ( interestrate / 100) * parseInt(noofyears) ) ) 
-                        - parseInt(amount);
-    document.getElementById("firstline").innerHTML = 
-            'If you deposit '.concat(amount).concat(',');
-    document.getElementById("secondline").innerHTML = 
-            'at an interest rate of '.concat(interestrate).concat("%.");
-    document.getElementById("thirdline").innerHTML = 
-            'You will receive an amount of '.concat(parseInt(interest)).concat(',');
-    let currentyear = new Date().getFullYear();
-    document.getElementById("fourthline").innerHTML = 
-            'in the year '.concat(parseInt(noofyears)+currentyear);
+    if(amount<=0){
+        alert("Enter a positive Amount")
+        document.getElementById("principal").focus();
+    }else{
+        let noofyears = document.getElementById("noofyears").value;
+        let interestrate = document.getElementById("inRange").value;
+        let interest = 0;
+    /*     for (let index = 1; index <= noofyears ; index++) {
+            total = total + ( total * ( interestrate / 100 ) );
+        } */
+        interest = ( parseInt(amount) * ( 1 + ( interestrate / 100) * parseInt(noofyears) ) ) 
+                            - parseInt(amount);
+        document.getElementById("firstline").innerHTML = 
+                'If you deposit '.concat(amount).concat(',');
+        document.getElementById("secondline").innerHTML = 
+                'at an interest rate of '.concat(interestrate).concat("%.");
+        document.getElementById("thirdline").innerHTML = 
+                'You will receive an amount of '.concat(parseInt(interest)).concat(',');
+        let currentyear = new Date().getFullYear();
+        document.getElementById("fourthline").innerHTML = 
+                'in the year '.concat(parseInt(noofyears)+currentyear);
+    }
 }
 
 
