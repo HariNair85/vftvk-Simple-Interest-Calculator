689bd20f583511c506edb4e2e249be95df8281ca
Harikrishnan M <40897366+HariNair85@users.noreply.github.com>
2022-03-29 22:51:12 +0530

    Submission

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let amount = document.getElementById("principal").value;
+    let noofyears = document.getElementById("noofyears").value;
+    let interestrate = document.getElementById("inRange").value;
+    let total = parseFloat(amount);
+    for (let index = 1; index <= noofyears ; index++) {
+        total = total + ( total * ( interestrate / 100 ) );
+    }
+    document.getElementById("firstline").innerHTML = 
+            'If you deposit '.concat(amount);
+    document.getElementById("secondline").innerHTML = 
+            'at an interest rate of '.concat(interestrate).concat("%");
+    document.getElementById("thirdline").innerHTML = 
+            'You will receive an amount of '.concat(total);
+    let currentyear = new Date().getFullYear();
+    document.getElementById("fourthline").innerHTML = 
+            'in the year '.concat(parseInt(noofyears)+currentyear);
 }
 
+
 function showRange(self){
-    document.querySelector("#setRange").addEventListener("change", 
-    function(self){
-        document.querySelector(".setRange").textContent=self.value;
-    }
-    )
+    let slider = document.getElementById("inRange").value;
+    let output = document.getElementById("setRange");
+    output.innerHTML = slider.concat("%");
 }
         
