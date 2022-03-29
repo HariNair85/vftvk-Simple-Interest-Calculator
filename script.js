89a8a3d3c68d3cdb2a76f641fb43f6400539f6bb
Harikrishnan M <40897366+HariNair85@users.noreply.github.com>
2022-03-29 23:06:35 +0530

    Final Submission

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ function compute()
     let amount = document.getElementById("principal").value;
     let noofyears = document.getElementById("noofyears").value;
     let interestrate = document.getElementById("inRange").value;
-    let total = parseFloat(amount);
-    for (let index = 1; index <= noofyears ; index++) {
+    let interest = 0;
+/*     for (let index = 1; index <= noofyears ; index++) {
         total = total + ( total * ( interestrate / 100 ) );
-    }
+    } */
+    interest = ( parseInt(amount) * ( 1 + ( interestrate / 100) * parseInt(noofyears) ) ) 
+                        - parseInt(amount);
     document.getElementById("firstline").innerHTML = 
             'If you deposit '.concat(amount);
     document.getElementById("secondline").innerHTML = 
             'at an interest rate of '.concat(interestrate).concat("%");
     document.getElementById("thirdline").innerHTML = 
-            'You will receive an amount of '.concat(total);
+            'You will receive an amount of '.concat(parseInt(interest));
     let currentyear = new Date().getFullYear();
     document.getElementById("fourthline").innerHTML = 
             'in the year '.concat(parseInt(noofyears)+currentyear);
