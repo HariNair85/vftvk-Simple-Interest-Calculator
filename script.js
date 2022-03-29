494f9e1c87b1904db64236a13a9acbb1e432ae8a
Harikrishnan M <40897366+HariNair85@users.noreply.github.com>
2022-03-29 21:37:56 +0530

    Update all changes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+}
+
+function showRange(self){
+    document.querySelector("#setRange").addEventListener("change", 
+    function(self){
+        document.querySelector(".setRange").textContent=self.value;
+    }
+    )
 }
         
